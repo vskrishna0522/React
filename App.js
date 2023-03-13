ef12ab5631f7f76181065a44f27ba440020ac318
vskrishna0522 <120077783+vskrishna0522@users.noreply.github.com>
2023-03-13 23:04:28 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+//syntax of function in react// 
+function App() {
+  return (
+    <h1>good hhhhing</h1>
+  );
+}
+export default App;
+
+//syntax of class in react
+class app extends React.Component{
+  render(){
+    return(
+      <h2>this is classs </h2>
+    )
+  }
+}
+export default app
+
