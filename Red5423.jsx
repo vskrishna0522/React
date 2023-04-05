4a0919e0e84fdc290097e0d8aeaf70dc16ae985b
Krishna <120077783+vskrishna0522@users.noreply.github.com>
2023-04-05 16:02:38 +0530

    Add files via upload

---
+++ Red5423.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Yellowtwo5423 from "./Yellowtwo5423";
+class Red5423 extends React.Component{
+    favaColur = "PURPLE"
+    belovedColour = "GREEN"    
+    render(){
+        return <div>
+            <h1>PROPS DRILLING 1</h1>
+            <Yellowtwo5423 one={"family"} two={"friends"} />
+        </div>
+    }
+}
+export default Red5423
---
+++ Yellowtwo5423.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+class Yellowtwo5423 extends React.Component{
+    render(){
+        return <div>
+            <h2> PROPS DRILLING 2</h2>
+            <pre>{JSON.stringify(this.props)}</pre>
+        </div>
+    }
+}
+export default Yellowtwo5423
