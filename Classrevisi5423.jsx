a17a10fc748019ae643aa67dd3145e6795f3c454
Krishna <120077783+vskrishna0522@users.noreply.github.com>
2023-04-05 11:42:58 +0530

    Add files via upload

---
+++ Classrevisi5423.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+class Classrevisi5423 extends React.Component{
+    render (){
+        return <nav className="navbar navbar-dark bg-warning expand-lg"> 
+         <a href="a" className="navbar-brand">Copier Needs</a>
+            <div className="ml-auto">
+                <ul className="navbar-nav">
+                    <li className="nav-list">
+                        <a className="nav-link" href="#">Toner </a>
+                    </li>
+                    <li className="nav-list">
+                        <a className="nav-link" href="#">Catridge</a>
+                    </li>
+                    <li className="nav-list">
+                        <a className="nav-link" href="#">Accessories</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    }
+}
+export default Classrevisi5423
