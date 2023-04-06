8d2b69b5d5fe0523a317426ea94fcb937a198d86
Krishna <120077783+vskrishna0522@users.noreply.github.com>
2023-04-06 11:59:15 +0530

    Add files via upload

---
+++ message.action.js
@@ -0,0 +1,9 @@
+let GM = 'namasthei ji'
+let GN = 'dhaniyawad ji'
+let gmAction = () => {
+    return { type : GM, vannakam}
+}
+let gnAction = () => {
+    return { type : GN, poietuva}
+}
+export { gmAction, gnAction, GM, GN }
